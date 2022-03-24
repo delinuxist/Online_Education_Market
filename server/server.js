ca851b6cb7d44dc5e145fb99daf3fa8504a34c38
delinuxist <delinuxist.gmail.com>
2022-03-24 13:58:45 +0000

    setup express,installed middlewares and routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
 import { ready, success } from "consola";
+import { readdirSync } from "fs";
+import express from "express";
+import cors from "cors";
+const morgan = require("morgan");
+require("dotenv").config();
 
-ready({
-  message: "Server ready",
-  badge: true,
+// create express App
+const app = express();
+
+// apply middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+readdirSync("./routes").map((r) => {
+  app.use("/api", require(`./routes/${r}`));
 });
+
+// port
+const port = process.env.PORT || 4000;
+
+app.listen(
+  port,
+  success({
+    message: `Server is running on port ${port}`,
+    badge: true,
+  })
+);
