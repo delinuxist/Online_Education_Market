f269a6d76e4c9ac846bc83470b8875e24400b719
delinuxist <delinuxist.gmail.com>
2022-03-24 08:56:50 +0000

    client initial setup

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="block w-32 text-2xl text-center text-white rounded bg-slate-800">
+        Hello
+      </div>
+    </>
+  );
+}
