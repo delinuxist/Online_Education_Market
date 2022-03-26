0548accefff50653eeb9cf0a7637854102ea3cfc
delinuxist <delinuxist.gmail.com>
2022-03-26 05:19:26 +0000

    added react-toastify component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,12 @@
 import Navbar from "../components/Layout/Navbar";
 import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <ToastContainer position="top-center" />
       <Navbar />
       <Component {...pageProps} />
     </>
