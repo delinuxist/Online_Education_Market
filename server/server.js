a423eda6efd07965907945c7bfddc97c824139b2
delinuxist <delinuxist.gmail.com>
2022-06-14 23:19:45 +0000

    code refactored and optimized

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
-import { ready, success } from "consola";
-import { readdirSync } from "fs";
-import express from "express";
-import cors from "cors";
+const { ready, success, error } = require("consola");
+const { readdirSync } = require("fs");
+const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
+const connectDb = require("./config/db.config");
+const errorHandler = require("./middlewares/errorHandler");
 require("dotenv").config();
+require("express-async-errors");
 
-// create express App
+// port
+const port = process.env.PORT || process.env.port;
+//database Uri
+const dbUri = process.env.DBURI;
+
+// create express instance
 const app = express();
 
 // apply middlewares
@@ -13,18 +21,31 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+  res.send("Welcome To Our Online Educational Market Api!!!");
+});
+
 //routes
 readdirSync("./routes").map((r) => {
   app.use("/api", require(`./routes/${r}`));
 });
 
-// port
-const port = process.env.PORT || 4000;
+// custom middlewares
+app.use(errorHandler);
 
-app.listen(
-  port,
-  success({
-    message: `Server is running on port ${port}`,
+const start = async () => {
+  await connectDb(dbUri);
+  ready({
+    message: "🚀 Database Connected...",
     badge: true,
-  })
-);
+  });
+  app.listen(
+    port,
+    success({
+      message: `🚀 Server running on port ${port}`,
+      badge: true,
+    })
+  );
+};
+
+start();
