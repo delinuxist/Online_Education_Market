089cc4929293df311516fdb17ae9eae17fde4c9b
delinuxist <delinuxist@gmail.com>
2022-06-20 22:41:12 +0000

    aws config file

---
+++ server/config/aws.config.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const AWS = require("aws-sdk");
+
+const awsConfig = {
+  accessKeyId: process.env.AWS_SECRET_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+const SES = new AWS.SES(awsConfig);
+
+module.exports = SES;
