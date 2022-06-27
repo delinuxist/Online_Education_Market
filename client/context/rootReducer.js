ec3de249fcd40e7f21ae04b44fdd057fb7747dd0
delinuxist <delinuxist@gmail.com>
2022-06-27 21:41:55 +0000

    context setup

---
+++ client/context/rootReducer.js
@@ -0,0 +1,16 @@
+import { COURSE, LOGIN, LOGOUT, OPEN } from "./types";
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return { ...state, user: action.payload };
+    case LOGOUT:
+      return { ...state, user: null };
+    case OPEN:
+      return { ...state, open: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
