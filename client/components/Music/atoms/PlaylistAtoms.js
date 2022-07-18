46b8a27f3baf776c5872ced00cd260db32ea20fd
delinuxist <delinuxist.gmail.com>
2022-07-18 23:13:05 +0000

    recoil state manager for playlist

---
+++ client/components/Music/atoms/PlaylistAtoms.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const PlaylistState = atom({
+  key: "playlistSpecialKey",
+  default: null,
+});
+
+export const PlaylistIdState = atom({
+  key: "playlistIdState",
+  default: "47utEZcgUr4IdbUDljHPCF",
+});
