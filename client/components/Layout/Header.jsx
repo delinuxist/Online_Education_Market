e3815b4639d1d1a27cd591efbcbdcc7c68a5c219
delinuxist <delinuxist@gmail.com>
2022-08-11 00:01:09 +0000

    Header component

---
+++ client/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import bg from "../../public/assets/img/milad-fakurian-FTrDy_oxjmA-unsplash.jpg";
+import React from "react";
+
+const Header = ({ heading }) => {
+  return (
+    <div className={`relative w-full  h-32`}>
+      <Image src={bg} alt="bg" priority layout="fill" />
+      <div className="absolute top-0 w-full h-full bg-black/50" />
+      <div
+        className={`absolute flex items-center justify-center w-full h-full text-white text-6xl font-bold
+        `}
+      >
+        <h1>{heading}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
