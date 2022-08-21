8e9b5ad0ddc3110331cfda114e58e8b78b184ce1
delinuxist <delinuxist@gmail.com>
2022-08-21 14:37:36 +0000

    register page

---
+++ client/pages/register.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+import Register from "../components/Auth/Register";
+
+const register = () => {
+  return <Register />;
+};
+
+export default register;
