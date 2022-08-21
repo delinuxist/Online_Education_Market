2018f7c0906d7e1aaa15dbdb2b1e3c45c979c555
delinuxist <delinuxist@gmail.com>
2022-08-21 00:27:07 +0000

    Navbar component

---
+++ client/components/Layout/Navbar.jsx
@@ -0,0 +1,199 @@
+import Link from "next/link";
+import { Fragment, useContext } from "react";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { Context } from "../../context/index";
+import { LOGOUT } from "../../context/types";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import axios from "axios";
+import defaultProfile from "../../public/assets/img/default.png";
+import { Menu, Transition } from "@headlessui/react";
+import Image from "next/image";
+
+const Navbar = () => {
+  const router = useRouter();
+
+  // access state
+  const { state, dispatch } = useContext(Context);
+
+  // destructuring user from state
+  const { user } = state;
+
+  // logout function
+  const logout = async () => {
+    // sending a get request to backend
+    const { data } = await axios.get("/server/logout");
+    if (data.success) {
+      // dispatch logout type
+      dispatch({
+        type: LOGOUT,
+      });
+
+      // remove user data from localStorage
+      window.localStorage.removeItem("user");
+
+      // redirect to login page
+      router.push("/login");
+
+      toast.success(data.msg);
+    }
+  };
+
+  return (
+    // <main className="relative antialiased bg-white dark:bg-[#25292A] z-0 overflow-hidden">
+    <nav className="flex fixed flex-row items-center justify-between w-screen px-4 md:px-20 h-20 antialiased bg-[#25292A] z-[100] shadow-lg shadow-[#25292A]/40">
+      <div className="relative flex flex-row items-center space-x-1">
+        <FaChalkboardTeacher className="text-4xl text-white" />
+        <Link href={`${user ? "/home" : "/"}`} passHref>
+          <h1 className="pl-2 text-2xl italic font-bold cursor-pointer text-gray-50">
+            J-LEARN
+          </h1>
+        </Link>
+      </div>
+      <div className="flex flex-row items-center space-x-8">
+        {user && user.role && user.role.includes("Instructor") ? (
+          <Link href="/instructor">
+            <a className="hidden font-semibold text-gray-200 transition duration-100 md:block hover:text-[#5BC0BE]">
+              Instructor
+            </a>
+          </Link>
+        ) : (
+          !user && (
+            <Link href="/#courses">
+              <a className="hidden font-semibold text-gray-200 transition duration-100 md:block hover:text-[#5BC0BE]">
+                Courses
+              </a>
+            </Link>
+          )
+        )}
+
+        {user && user.role && !user.role.includes("Instructor") ? (
+          <Link href="/user/become-instructor">
+            <a className="hidden font-semibold text-gray-200 transition duration-100 md:block hover:text-[#5BC0BE]">
+              Become Instructor
+            </a>
+          </Link>
+        ) : (
+          user &&
+          user.role &&
+          user.role.includes("Instructor") && (
+            <Link href="/instructor/course/create">
+              <a className="hidden font-semibold text-gray-200 transition duration-100 md:block hover:text-[#5BC0BE]">
+                Create Course
+              </a>
+            </Link>
+          )
+        )}
+
+        {user === null && (
+          <>
+            <div className="relative hidden px-8 py-2 bg-white border border-gray-800 md:block rounded-3xl ">
+              <div
+                className="absolute inset-0 px-8 py-2 transform translate-x-1 translate-y-2 border rounded-3xl border-gray-50"
+                style={{ zIndex: -10 }}
+              ></div>
+              <Link href="/login">
+                <a className="relative font-bold text-gray-700 transition duration-100 transform hover:text-green-500 hover:-translate-y-2 hover:-translate-x-1">
+                  Login
+                </a>
+              </Link>
+            </div>
+            <div className="relative hidden px-8 py-2 bg-white border border-gray-800 md:block rounded-3xl ">
+              <div
+                className="absolute inset-0 px-8 py-2 transform translate-x-1 translate-y-2 border rounded-3xl border-gray-50"
+                style={{ zIndex: -10 }}
+              ></div>
+              <Link href="/register">
+                <a className="relative font-bold text-gray-700 transition duration-100 transform hover:text-green-500 hover:-translate-y-2 hover:-translate-x-1">
+                  Register
+                </a>
+              </Link>
+            </div>
+          </>
+        )}
+
+        {user !== null && (
+          <div className="flex items-center justify-between text-white ">
+            <h1>{user && user.name}</h1>
+            <Menu
+              as="div"
+              className=" relative cursor-pointer w-[50px] rounded-full h-[50px] ml-8 bg-slate-400"
+            >
+              {({ open }) => (
+                <>
+                  <Menu.Button>
+                    <Image
+                      className="rounded-full "
+                      src={defaultProfile}
+                      alt="profile"
+                    />
+                  </Menu.Button>
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute origin-top-right rounded-md right-0 bg-[#3A506B] h-[90px] w-[120px]">
+                      <div className="flex flex-col items-center w-full h-full py-2">
+                        {user && !user.role.includes("Instructor") && (
+                          <Link href="/user" passHref>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <button
+                                  className={`w-full font-bold ${
+                                    active ? "bg-[#5BC0BE]" : "text-gray-400"
+                                  }`}
+                                >
+                                  Dashboard
+                                </button>
+                              )}
+                            </Menu.Item>
+                          </Link>
+                        )}
+
+                        <Link href="/dashboard" passHref>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                className={`w-full font-bold ${
+                                  active ? "bg-[#5BC0BE]" : "text-gray-400"
+                                }`}
+                              >
+                                Account
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </Link>
+
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              onClick={logout}
+                              className={`font-bold w-full ${
+                                active ? "bg-[#5BC0BE]" : "text-gray-400"
+                              }`}
+                            >
+                              Logout
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </>
+              )}
+            </Menu>
+          </div>
+        )}
+      </div>
+    </nav>
+    // </main>
+  );
+};
+
+export default Navbar;
