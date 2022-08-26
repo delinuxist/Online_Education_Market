76de541e5fe401b247ca03b43f287d0de9cbccdb
delinuxist <delinuxist@gmail.com>
2022-08-26 17:12:31 +0000

    course model

---
+++ server/models/course.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+// const lessonSchema = require("./lesson");
+
+const Schema = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
+
+const lessonSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      minlength: 5,
+      maxlength: 320,
+      required: true,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    content: {
+      type: {},
+      minlength: 200,
+    },
+    numberOfQuestions: {
+      type: Number,
+      default: 0,
+    },
+    testAdded: {
+      type: Boolean,
+      default: false,
+    },
+    video: {},
+    free_preview: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const courseSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: 5,
+      maxlength: 320,
+      required: true,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    description: {
+      type: {},
+      minlength: 200,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0.0,
+    },
+    image: {},
+    category: {
+      type: String,
+    },
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    upcoming: {
+      type: Boolean,
+      default: false,
+    },
+    paid: {
+      type: Boolean,
+      default: false,
+    },
+    instructor: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+    lessons: [lessonSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("course", courseSchema);
