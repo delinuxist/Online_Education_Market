d1d2f0cc61acb3c32ed78edf64616039034aeae8
Emmanuel Obeng Twene <delinuxist@gmail.com>
2022-10-16 19:24:37 +0000

    footer component

---
+++ client/components/home/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-32 bg-slate-600">
+      <div className="flex items-center justify-center w-full h-full text-white ">
+        <div className="px-4 py-2 text-4xl hover:border-2 border-[#5BC0BE] font-bold rounded-lg duration-300">
+          Join Now
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
