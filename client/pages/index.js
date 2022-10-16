52a2bf15285a062af608c3b983305abac7926cf2
Emmanuel Obeng Twene <delinuxist@gmail.com>
2022-10-16 17:46:20 +0000

    home page when site loads

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,49 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useContext, useEffect } from "react";
+import Link from "next/link";
+import Hero from "../components/home/Hero";
+import Courses from "../components/home/Courses";
+import About from "../components/home/About";
+import Footer from "../components/home/Footer";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ courses, upcoming }) {
+  const {
+    state: { user },
+  } = useContext(Context);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user !== null) {
+      router.push("/home");
+    }
+  }, [router, user]);
+
   return (
     <>
-      <div className="block w-32 text-2xl text-center text-white rounded bg-slate-800">
-        Hello
-      </div>
+      <Hero />
+      <Courses courses={courses} upcoming={upcoming} />
+      <About />
+      <Footer />
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(
+    `${process.env.API}/course/published-courses`
+  );
+  const res = await axios.get(`${process.env.API}/course/upcoming-courses`);
+
+  // console.log(res.data.courses);
+
+  return {
+    props: {
+      courses: data.courses,
+      upcoming: res.data.courses,
+    },
+  };
 }
