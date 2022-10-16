93da92800dccd451b18db24d8737fabd04ec8822
Emmanuel Obeng Twene <delinuxist@gmail.com>
2022-10-16 17:16:51 +0000

    enrolled course card component

--- client/components/Course/EnrolledCourseCard.jsx
+++ client/components/Course/EnrolledCourseCard.jsx
@@ -1,14 +1,36 @@
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useState } from "react";
 import { FaPlayCircle } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import ConfirmModal from "../Layout/ConfirmModal";
 
-const EnrolledCourseCard = ({ course }) => {
+const EnrolledCourseCard = ({ course, courses, index, setCourses }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const deleteCourse = async () => {
+    try {
+      const myCourses = [...courses];
+      myCourses.splice(index, 1);
+      setCourses(myCourses);
+
+      const { data } = await axios.get(
+        `/server/user/deleteCourse/${course._id}`
+      );
+
+      setIsOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <Link href={`/user/course/${course.slug}`}>
-      <a className="w-full duration-300 border rounded-md shadow-lg h-36 border-slate-300 hover:scale-105">
-        <div className="flex items-center w-full h-full px-10">
-          <div className="flex items-center w-full h-full">
+    <div className="w-full duration-300 border rounded-md shadow-lg h-36 border-slate-300 hover:scale-105">
+      <div className="flex items-center w-full h-full px-10">
+        <Link href={`/user/course/${course.slug}`}>
+          <a className="flex items-center w-full h-full">
             <div className="w-32 h-32 mr-4 rounded-lg shadow-2xl">
               <Image
                 className="rounded-lg "
@@ -25,11 +47,22 @@ const EnrolledCourseCard = ({ course }) => {
               <p className="pt-2 text-xl font-light">{`${course?.lessons?.length} Lessons`}</p>
               <p className="pt-2 font-light text-gray-500">{`Created by: ${course.instructor.name}`}</p>
             </div>
-          </div>
-          <FaPlayCircle size={40} className=" text-[#5BC0BE]" />
-        </div>
-      </a>
-    </Link>
+          </a>
+        </Link>
+        <MdDelete
+          onClick={() => setIsOpen(!isOpen)}
+          size={40}
+          className="text-red-600 duration-300 hover:scale-105 z-index-[100] cursor-pointer"
+        />
+        <ConfirmModal
+          isOpen={isOpen}
+          command={deleteCourse}
+          setIsOpen={setIsOpen}
+          title={"Delete Course"}
+          message={"Are you sure you want to delete course from list"}
+        />
+      </div>
+    </div>
   );
 };
 
