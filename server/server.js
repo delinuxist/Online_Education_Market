d7d3c50fb34e800424c0efc6243b4805520850ac
Emmanuel Obeng Twene <delinuxist@gmail.com>
2022-10-16 17:43:17 +0000

    main server

--- server/server.js
+++ server/server.js
@@ -3,10 +3,17 @@ const { readdirSync } = require("fs");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const connectDb = require("./config/db.config");
 const errorHandler = require("./middlewares/errorHandler");
 require("dotenv").config();
 require("express-async-errors");
+
+// router import
+// const InstructorRoutes = require("./routes/instructor.routes");
+// const AuthRoutes = require("./routes/auth.routes");
+// const CourseRoutes = require("./routes/course.routes");
+// const UserRoutes = require("./routes/user.routes");
 
 // port
 const port = process.env.PORT || process.env.port;
@@ -18,8 +25,9 @@ const app = express();
 
 // apply middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome To Our Online Educational Market Api!!!");
@@ -27,8 +35,12 @@ app.get("/", (req, res) => {
 
 //routes
 readdirSync("./routes").map((r) => {
-  app.use("/api", require(`./routes/${r}`));
+  app.use("/server", require(`./routes/${r}`));
 });
+// app.use("/api", AuthRoutes);
+// app.use("/api", UserRoutes);
+// app.use("/api", InstructorRoutes);
+// app.use("/api", CourseRoutes);
 
 // custom middlewares
 app.use(errorHandler);
